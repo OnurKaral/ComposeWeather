013f88a556d55f7c3de4b16e9b3af128f5931cdc
OnurKaral <santobixua@gmail.com>
2022-07-25 14:14:34 +0300

    SplashScreen added with navigation

---
+++ app/src/main/java/com/example/composeweather/navigation/WeatherScreens.kt
@@ -0,0 +1,11 @@
+package com.example.composeweather.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavoriteScreen,
+    SearchScreen,
+    SettingsScreen,
+
+}
