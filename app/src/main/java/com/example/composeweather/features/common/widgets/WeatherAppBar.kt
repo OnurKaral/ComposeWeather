012b4c56f1c5241a84bd954b3479c8ac7b495562
OnurKaral <santobixua@gmail.com>
2022-07-28 16:40:01 +0300

    dialog

--- app/src/main/java/com/example/composeweather/features/common/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/composeweather/features/common/widgets/WeatherAppBar.kt
@@ -1,19 +1,25 @@
 package com.example.composeweather.features.common.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.composeweather.navigation.WeatherScreens
 
 
 @Composable
@@ -24,8 +30,15 @@ fun WeatherAppBar(
     elevation: Dp = 0.dp,
     navController: NavController,
     onAddActionClicked: () -> Unit = {},
-    onButtonClicked: () -> Unit= {}) {
-        
+    onButtonClicked: () -> Unit= {
+
+    }) {
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+    if (showDialog.value) {
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+    }
     TopAppBar(title = {
                       Text(text = title)
     }, actions = {
@@ -34,7 +47,10 @@ fun WeatherAppBar(
                         Icon(imageVector = Icons.Default.Search,
                             contentDescription = "Search")
                      }
-                     IconButton(onClick = { /*TODO*/ }) {
+                     IconButton(onClick = {
+                         showDialog.value = true
+
+                     }) {
                          Icon(imageVector = Icons.Rounded.MoreVert,
                              contentDescription = "More")
                      }
@@ -53,7 +69,51 @@ fun WeatherAppBar(
     elevation = elevation)
 }
 
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>,
+                            navController: NavController) {
+    var expanded by remember { mutableStateOf(true) }
+    val items = listOf("About", "Favorites", "Settings")
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.TopEnd)
+        .absolutePadding(top = 45.dp, right = 20.dp)) {
+        DropdownMenu(expanded = expanded ,
+            onDismissRequest = { expanded = false},
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(
+                        imageVector = when (text) {
+                            "About" -> Icons.Default.Info
+                            "Favorites" -> Icons.Default.FavoriteBorder
+                            else -> Icons.Default.Settings
 
+                        }, contentDescription = null,
+                        tint = Color.LightGray
+                    )
+                    Text(text = text,
+                        modifier = Modifier.clickable {
+                            navController.navigate(
+                                when (text) {
+                                    "About" -> WeatherScreens.AboutScreen.name
+                                    "Favorites" -> WeatherScreens.FavoriteScreen.name
+                                    else -> WeatherScreens.SettingsScreen.name
+                                })
+
+
+                        }, fontWeight = FontWeight.W300)
+
+                }
+            }
+        }
+    }
+}
 
 
 
