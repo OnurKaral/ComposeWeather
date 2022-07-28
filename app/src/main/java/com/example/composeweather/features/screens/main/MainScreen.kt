0200567245ec0efdcb0d315d0c2fedc73257fbf1
OnurKaral <santobixua@gmail.com>
2022-07-28 10:43:06 +0300

    mainScreen HumidityWindPressure

--- app/src/main/java/com/example/composeweather/features/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/composeweather/features/screens/main/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
@@ -91,9 +92,47 @@ fun MainContent(data: WeatherResponse) {
                                         modifier = Modifier.padding(4.dp))
 
 
+
                        }
+
                }
+           HumidityWindPressureRow(data = data)
+           Divider()
        }
+}
+
+@Composable
+fun HumidityWindPressureRow(data: WeatherResponse) {
+    
+    Row(modifier = Modifier
+        .padding(12.dp)
+        .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween) {
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Humidity",
+                    modifier = Modifier.size(20.dp))
+
+            Text(text = "${data.main.humidity}%")
+        }
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Feels like",
+                modifier = Modifier.size(20.dp))
+
+            Text(text = "${data.main.feels_like}%")
+        }
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Pressure",
+                modifier = Modifier.size(20.dp))
+
+            Text(text = "${data.main.pressure}%")
+        }
+
+    }
+
+
+
 }
 
 @Composable
